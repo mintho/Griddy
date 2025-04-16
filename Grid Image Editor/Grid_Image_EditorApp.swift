a66773ff5d517284a3e2dc734b48ff2221dc126c
Miskatonic University Press <contact@miskatonic-university-press.com>
2025-04-16 23:47:20 +0200

    Initial Commit

---
+++ Grid Image Editor/Grid_Image_EditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Grid_Image_EditorApp.swift
+//  Grid Image Editor
+//
+//  Created by Thomas Minzenmay on 16.04.25.
+//
+
+import SwiftUI
+
+@main
+struct Grid_Image_EditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Grid Image EditorTests/Grid_Image_EditorTests.swift
@@ -0,0 +1,17 @@
+//
+//  Grid_Image_EditorTests.swift
+//  Grid Image EditorTests
+//
+//  Created by Thomas Minzenmay on 16.04.25.
+//
+
+import Testing
+@testable import Grid_Image_Editor
+
+struct Grid_Image_EditorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
